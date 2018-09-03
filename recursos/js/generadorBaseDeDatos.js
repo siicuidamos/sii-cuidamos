0961c18d71c183fd57b64d17c6abdf6efa8432ed
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-09-03 18:49:54 -0500

    La base de información para el desarrollo del proyecto

---
+++ recursos/js/generadorBaseDeDatos.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+let jsonLinks = require('../json/BaseDeDatosVideos.json');
+let json = require('../json/EnlacesProyectosPorEntidad.json')['rows'];
+
+for (let x = 0; x < jsonLinks.length; x++) {
+    let bpin = jsonLinks[x]['bpin'];
+    let link = jsonLinks[x]['link'];
+
+    for (let j = 0; j < json.length; j++) {
+        const bpinJ = json[j]['bpin'];
+        if (bpinJ === bpin) {
+            json[j]['link'] = link;
+        }
+    }
+
+}
+
+var data = JSON.stringify(json);
+var fs = require('fs');
+fs.writeFileSync("../json/Proyectos.json", data);
