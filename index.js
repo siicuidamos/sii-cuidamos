7a8269480699048a799d15a4aef8ba9316323fe3
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-09-03 22:20:50 -0500

    Se documenta correctamente y se agrega la configuración de la base de datos

---
+++ index.js
@@ -0,0 +1,85 @@
+// Archivo que permite configurar ciertos elementos a partir del ambiente en el que se corre el proyecto
+const env = require('./env');
+
+// Se usa Express, una infraestructura web rápida, minimalista y flexible para Node.js
+const express = require("express");
+
+// Se usa Mongoose, una herramienta de modelado de objetos de MongoDB diseñada para trabajar en un ambiente asíncrono
+const mongoose = require("mongoose");
+
+// Se usa Body-Parser, se encarga de analizar los cuerpos (body) de las solicitudes en el middleware antes que nuestros manejadores lo hagan
+const bodyParser = require("body-parser");
+
+// Se usa CORS, que permite proveer un middleware Connect/Express que puede ser usado para habilitar CORS con varias opciones
+const cors = require("cors");
+
+// Puerto a usar. Se tiene en cuenta el caso de Heroku
+const port = process.env.PORT || 8080;
+
+// Se obtiene la configuración de la base de datos
+const config = require("./config/database");
+
+// Inicializacion de Express
+const app = express();
+
+// Se define la promesa global de Moongose
+mongoose.Promise = global.Promise;
+
+//Conexión a base de datos
+mongoose.connect(
+    config.uri, {
+        useNewUrlParser: true
+    },
+    err => {
+        // Verifica que haya sido posible conectarse a la base de datos. Lanza el mensaje correspondiente
+        if (err) {
+            console.log("Hubo un error al conectarse a la base de datos " + err);
+        } else {
+            console.log(
+                'La conexión con la base de datos "' +
+                config.db +
+                '" se realizó satisfactoriamente'
+            );
+        }
+    }
+);
+
+// Se habilita CORS en el puerto 4200. Donde corre el cliente.
+app.use(
+    cors({
+        origin: "http://localhost:4200"
+    })
+);
+
+// Configura las peticiones con cuerpo de tipo JSON
+app.use(
+    bodyParser.json({
+        limit: "1mb"
+    })
+);
+
+// Configura las peticiones con cuerpo de tipo url encoded
+app.use(
+    bodyParser.urlencoded({
+        limit: "1mb",
+        extended: true
+    })
+);
+
+// Se define la ruta de archivos estáticos para Express
+app.use(express.static(__dirname + "/public"));
+
+//Se redirige desde cualquier página al 'index.html'. Esto se hace al inicio de la aplicación
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+//Se define el puerto en el que trabaja el servidor
+app.listen(port, () => {
+    console.log(
+        "El servidor de la aplicación está corriendo en el puerto " +
+        port +
+        " en modo " +
+        process.env.NODE_ENV
+    );
+});
