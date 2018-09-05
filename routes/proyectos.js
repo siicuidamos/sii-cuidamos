3c0dd1372be9d42d1482f10ab8389906fa56bf01
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-09-05 13:11:52 -0500

    Buscar un proyecto por BPIN

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,22 +1,21 @@
 // Modelo de un proyecto
-const Proyecto = require('../models/proyecto');
+const Proyecto = require("../models/proyecto");
 
-module.exports = (router) => {
-
+module.exports = router => {
     // API para obtener todos los proyectos a partir de la pagina
-    router.get('/proyectos/:pagina', (req, res) => {
+    router.get("/proyectos/:pagina", (req, res) => {
         let pagina = req.params.pagina || 0;
         let limite = pagina * 10;
         Proyecto.find({},
             null, {
                 skip: limite,
-                limit: 10,
+                limit: 10
             },
             (err, proyectos) => {
                 if (err) {
                     res.json({
                         exito: false,
-                        mensaje: err
+                        mensaje: "Se presentó un error en la consulta. Error: " + err
                     });
                 } else {
                     if (!proyectos) {
@@ -27,7 +26,7 @@ module.exports = (router) => {
                     } else if (proyectos.length === 0) {
                         res.json({
                             exito: false,
-                            proyectos: 'Se supera el valor máximo de los datos.'
+                            proyectos: "Se supera el valor máximo de los datos."
                         });
                     } else {
                         res.json({
@@ -38,7 +37,44 @@ module.exports = (router) => {
                 }
             }
         );
-
     });
+
+    // API para obtener un proyecto por BPIN
+    router.get("/proyectos/bpin/:bpin", (req, res) => {
+        let bpin = req.params.bpin;
+        if (!bpin) {
+            res.json({
+                exito: false,
+                mensaje: "Debe ingresar un código BPIN para realizar las busquedas."
+            });
+        } else if (!bpin.match(/^[0-9]+$/)) {
+            res.json({
+                exito: false,
+                mensaje: "El BPIN solo puede ser un número."
+            });
+        } else {
+            Proyecto.findOne({
+                bpin: bpin
+            }, (err, proyecto) => {
+                if (err) {
+                    res.json({
+                        exito: false,
+                        mensaje: "Se presentó un error en la consulta. Error: " + err
+                    });
+                } else if (!proyecto) {
+                    res.json({
+                        exito: false,
+                        mensaje: "No existe un proyecto asociado al BPIN " + bpin
+                    });
+                } else {
+                    res.json({
+                        exito: true,
+                        proyecto: proyecto
+                    });
+                }
+            });
+        }
+    });
+
     return router;
 };
