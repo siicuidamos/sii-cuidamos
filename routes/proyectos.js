3d87bfad28aee440c33f87057da59cefdb2aa061
vngomez <vn.gomez@uniandes.edu.co>
2018-09-06 17:44:43 -0500

    API obtener proyecto por departamento

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -76,5 +76,46 @@ module.exports = router => {
         }
     });
 
+    //API para filtrar por departamentos
+    router.get("/proyectos/departamento/:departamento/:pagina", (req, res) => {
+         let pagina = req.params.pagina || 0;
+        let limite = pagina * 10;
+        let departamento = req.params.departamento.toUpperCase();
+        departamento = departamento.replace("_"," ");
+        Proyecto.find({ departamento:departamento},
+            null, {
+                skip: limite,
+                limit: 10
+            },
+            (err, proyectos) => {
+                if (err) {
+                    res.json({
+                        exito: false,
+                        mensaje: "Se presentó un error en la consulta. Error: " + err
+                    });
+                } else if (!proyectos) {
+                    res.json({
+                        exito: false,
+                        mensaje: "No hay proyectos en la base de datos."
+                    });
+                } else if (proyectos.length === 0) {
+                    res.json({
+                        exito: false,
+                        proyectos: "Se supera el valor máximo de los datos."
+                    });
+                } else {
+                    res.json({
+                        exito: true,
+                        proyectos: proyectos
+                    });
+                }
+            }
+        );
+    });
+       
+
+
+
+
     return router;
 };
