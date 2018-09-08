9f6b888509712f33d3f0e0b9ee87f5c09e4fcf78
vngomez <vn.gomez@uniandes.edu.co>
2018-09-08 12:33:26 -0500

    Obtenerla calificación promedio de un proyecto, por bpin y categoria de comentario

--- routes/comentarios.js
+++ routes/comentarios.js
@@ -1,5 +1,6 @@
 // Modelo de un proyecto
 const Comentario = require("../models/comentario");
+const Proyecto = require("../models/proyecto");
 
 module.exports = router => {
   // API para crear un comentario nuevo
@@ -89,6 +90,34 @@ module.exports = router => {
       }
     }
   });
+
+  //API para obtener la calificación promedio de un proyecto, por bpin y categoria de comentario
+  router.get("/comentarios/calificacionPromedioXCategoria/bpin/:bpin/categoria/:categoria", (req, res) => {
+    let categoria = req.params.categoria;
+    let bpin = req.params.bpin;
+        let pagina = req.params.pagina || 0;
+        let limite = pagina * 10;
+        Comentario.aggregate([{ $match : { bpin:bpin, categoria : categoria} }, { $group: { _id: "$bpin", calificacionPromedio: {$avg: "$calificacion"} }}],
+            (err, calificacionPromedio) => {
+                if (err) {
+                    res.json({
+                        exito: false,
+                        mensaje: "Se presentó un error en la consulta. Error: " + err
+                    });
+                } else if (calificacionPromedio.length === 0) {
+                    res.json({
+                        exito: false,
+                        mensaje: "No hay resultados para los parámetros ingresados (bpin: "+ bpin+" , categoría: "+categoria+")"
+                    });
+                }else {
+                    res.json({
+                        exito: true,
+                        calificacionPromedio: calificacionPromedio
+                    });
+                }
+            }
+        );
+    });
 
   return router;
 };
