1ac001cc51d5e7332090702793255f3059e7a69b
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-09-07 22:11:45 -0500

    Modificaciones

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -2,7 +2,8 @@
 const Proyecto = require("../models/proyecto");
 
 module.exports = router => {
-    // API para obtener todos los proyectos a partir de la pagina
+
+    // API para obtener todos los proyectos con paginacion
     router.get("/proyectos/:pagina", (req, res) => {
         let pagina = req.params.pagina || 0;
         let limite = pagina * 10;
@@ -17,26 +18,68 @@ module.exports = router => {
                         exito: false,
                         mensaje: "Se presentó un error en la consulta. Error: " + err
                     });
+                } else if (!proyectos) {
+                    res.json({
+                        exito: false,
+                        mensaje: "No hay proyectos en la base de datos."
+                    });
+                } else if (proyectos.length === 0) {
+                    res.json({
+                        exito: false,
+                        proyectos: "Se supera el valor máximo de los datos."
+                    });
                 } else {
-                    if (!proyectos) {
-                        res.json({
-                            exito: false,
-                            mensaje: "No hay proyectos en la base de datos."
-                        });
-                    } else if (proyectos.length === 0) {
-                        res.json({
-                            exito: false,
-                            proyectos: "Se supera el valor máximo de los datos."
-                        });
-                    } else {
-                        res.json({
-                            exito: true,
-                            proyectos: proyectos
-                        });
-                    }
+                    res.json({
+                        exito: true,
+                        proyectos: proyectos
+                    });
                 }
             }
         );
+    });
+
+    // API para obtener los proyectos de un municipio con paginacion
+    router.get("/proyectos/municipio/:municipio/:pagina", (req, res) => {
+        let municipio = req.params.municipio.toUpperCase();
+        let pagina = req.params.pagina || 0;
+        let limite = pagina * 10;
+
+        if (!municipio) {
+            res.json({
+                exito: false,
+                mensaje: 'Debe seleccionar un municipio válido'
+            });
+        } else {
+            municipio = municipio.replace("_", " ");
+            Proyecto.find({
+                municipio: municipio
+            }, null, {
+                skip: limite,
+                limit: 10
+            }, (err, proyectos) => {
+                if (err) {
+                    res.json({
+                        exito: false,
+                        mensaje: 'Se presentó un error en la consulta. Error: ' + err
+                    })
+                } else if (!proyectos) {
+                    res.json({
+                        exito: false,
+                        mensaje: "No hay proyectos en el municipio " + municipio
+                    });
+                } else if (proyectos.length === 0) {
+                    res.json({
+                        exito: false,
+                        proyectos: "Se supera el valor máximo de los datos."
+                    });
+                } else {
+                    res.json({
+                        exito: true,
+                        proyectos: proyectos
+                    });
+                }
+            });
+        }
     });
 
     // API para obtener un proyecto por BPIN
