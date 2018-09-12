76b1776fed3051a44f135754c333937f1e04a2f4
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-09-11 23:22:35 -0500

    Se implementa un diseño más bonito

--- client/src/components/ProyectoGeneral.jsx
+++ client/src/components/ProyectoGeneral.jsx
@@ -9,6 +9,8 @@ class ProyectoGeneral extends Component {
     const departamento = proyecto.departamento;
     const municipio = proyecto.municipio;
     const sector = proyecto.sector;
+    const inicio = proyecto.anioInicioEjecucion;
+    const fin = proyecto.anioFinEjecucion;
 
     return (
       <div className="col-12 mt-4">
@@ -17,12 +19,14 @@ class ProyectoGeneral extends Component {
             <h5 className="card-title">{nombre}</h5>
             <hr />
             <p className="card-text">
-              <b>Región: </b> {region} - 
-              <b> Departamento: </b>{departamento} -
-              <b> Municipio: </b>{municipio}
+            <i className="fas fa-map-marker-alt fa-lg text-danger"></i>&nbsp;
+              {region}, {departamento}, {municipio}
             </p>
             <p className="card-text">
-                <b>Sector: </b> {sector}
+            <i className="fas fa-list-ul fa-lg text-primary"></i>&nbsp;{sector}
+            </p>
+            <p className="card-text">
+            <i className="fas fa-calendar fa-lg text-success"></i>&nbsp;{inicio} - {fin}
             </p>
           </div>
         </div>
