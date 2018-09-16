9730aa31cbd6b55d77fbeaf04d31ee6345665c5c
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-09-16 11:41:24 -0500

    Modificaciones

---
+++ env.js
@@ -0,0 +1,13 @@
+// Se usa Node.js File System Module, que permite trabajar con los archivos del sistema.
+const fs = require('fs');
+
+// Se mira si la carpeta public existe, ya que en ese caso se usa la configuración del modo en producción (production).
+if (fs.existsSync('client/build')) {
+    process.env.NODE_ENV = 'production';
+    process.env.databaseUri = process.env.MONGODB_URI;
+    process.env.databaseName = 'production database: proyectosveeduria';
+} else {
+    process.env.NODE_ENV = 'development';
+    process.env.databaseUri = 'mongodb://localhost:27017/proyectosveeduria'; // Base de datos real
+    process.env.databaseName = 'development database: proyectosveeduria';
+}
