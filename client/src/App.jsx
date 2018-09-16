506d152d33b33fe3117a11a77ab0769fcb104674
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-09-16 01:35:30 -0500

    Problema con modulo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 //import { BrowserRouter as Router, Route } from "react-router-dom";
 import Inicio from './components/Inicio';
-import Navbar from './components/Navbar';
+//import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
         <div className="container container-fluid mb-5 mt-1">
           <h1 className="text-center">Veeduría de proyectos públicos</h1>
           <Inicio />
