b18aef47ed87995ae0d3e8a1d95585b1347aa612
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-09-16 01:41:12 -0500

    Barra agregada

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 //import { BrowserRouter as Router, Route } from "react-router-dom";
 import Inicio from './components/Inicio';
-import Navbar from './components/Navbar';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <NavBar />
         <div className="container container-fluid mb-5 mt-1">
           <h1 className="text-center">Veeduría de proyectos públicos</h1>
           <Inicio />
