59381c342fe197be3064fddddb383faac79e27fa
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-09-16 23:03:13 -0500

    Se eliminan funciones no usadas actualmente

--- resources/js/ordenarJSON.js
+++
@@ -1,9 +0,0 @@
-const sectores = require('../../client/src/json/Sectores.json');
-const aniosInicio = require('../../client/src/json/AniosInicio.json');
-
-// Se usa el editor de archivos de NODE
-const fs = require('fs');
-
-
-fs.writeFileSync("../../client/src/json/Sectores.json", JSON.stringify(sectores.sort()));
-fs.writeFileSync("../../client/src/json/AniosInicio.json", JSON.stringify(aniosInicio.sort()));
