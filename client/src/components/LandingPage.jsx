043894f955d1e6539c50d54ff4c9c8b0b7029152
Vivian <vn.gomez@uniandes.edu.co>
2019-07-25 12:18:47 -0500

    Arreglos responsive landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -6,27 +6,18 @@ class LandingPage extends Component {
   render() {
     return (
       <div className="container-fluid">
-      <div className="row">
-        <div className="col-12 parent">
-        <img 
-                 width="100%"
-                 height="100%"
-                 src={require('../images/slides/ht1.jpg')}
-                 alt="gente hablando"
-                 className="responsive"
-                 />
-        </div>  
-        <div className="col-6 child-element-left">
+      <div className="row parent">
+        <div className="col-md-6 col-sm-12 child ">
         <h1 className="text-white"><b>Bienvenido</b></h1> 
         <hr/>
         <h5 className="text-white">Haz parte de si cuidamos, un lugar para participar y crear comunidad.</h5> 
         </div>
-        <div className="col-6  child-element-right">
+        <div className="col-md-6 col-sm-12 child">
         <div class="box3 sb14">Descubre cómo trabajamos
         <button className="btn btn-outline-light"><i class="fas fa-play"></i>&nbsp;Ver video</button>
         </div>
         </div>
-      </div>
+      </div> 
       </div>
     );
   }
