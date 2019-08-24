e7b2197a9bf302b6aee9ad9243135ac7bd989c5d
jcsanguino10 <jc.sanguino10@uniandes.edu.co>
2019-08-24 15:08:31 -0500

    modelo

--- env.js
+++ env.js
@@ -10,8 +10,9 @@ if (fs.existsSync('client/build')) {
   process.env.clientId = process.env.CLIENT_ID;
 } else {
   process.env.NODE_ENV = 'development';
-  process.env.databaseUri = 'mongodb://localhost:27017/proyectosveeduria'; // Base de datos real
+  process.env.databaseUri =
+    'mongodb://admin:lanata123@ds143932.mlab.com:43932/proyectosveeduria'; // Base de datos real
   process.env.databaseName = 'development database: proyectosveeduria';
-  process.env.userPoolId = '';
-  process.env.clientId = '';
+  process.env.userPoolId = 'us-east-1_JikbxviS2';
+  process.env.clientId = '7qan9ltj7k0jpqoel3e4sjbp5e';
 }
